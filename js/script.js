e436efe20b82fc7ea994c3bbbe396fd26114674f
kawearaujo <kawe.apr@gmail.com>
2021-05-27 12:22:12 -0300

    By Kawe

--- js/script.js
+++ js/script.js
@@ -1,10 +1,25 @@
-
-
-var bgnumber=1;
-var doc=document.getElementsByClassName('imagebg')
-console.log(doc.attributes)
-// function slide(n) {
-//      var maxbg =6;
-//      doc.= "img/1.jpg";
-//      // doc.style.backgroundImage= "url:('/img/"+n+".jpg');";   
-//  }
+window.onload(slide(4));
+var bg=1;
+function slide(n){
+    document.getElementById("portifolio").style= "background-image: url('/img/"+n+".jpg')"
+    botoes(n,3)
+    bg=n
+}
+function botoes(n,m){
+    document.getElementById('botoes').innerHTML = "";
+    for (a=1;a<=3;a++){
+        if(a==n){
+            document.getElementById('botoes').innerHTML+="<li class='selecionado' onclick='slide("+a+")'></li>";
+        }else{
+            document.getElementById('botoes').innerHTML+="<li onclick='slide("+a+")'></li>";
+        }
+    }
+    
+}
+function scroller(ident){
+    document.querySelector(ident).scrollIntoViewOptions({behavior: "smooth"});
+}
+document.querySelector("#down").addEventListener("onclick",function(event){
+    event.preventDefault();
+    scroller("#pag2");
+});
