a9274c35857fd03151f5281a12b2883932d02981
kawearaujo <kawe.apr@gmail.com>
2021-07-31 15:11:03 -0300

    by Kawe

--- js/script.js
+++ js/script.js
@@ -40,14 +40,7 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
-    
-    this.classList.toggle("active");
-    // var content = this.nextElementSibling;
-    // if (content.style.display === "flex") {
-    //   content.style.display = "none";
-    // } else {
-    //   content.style.display = "flex";
-    // }
+    //this.classList.toggle("active");
     switch (this.innerHTML) {
         case "Open Section 1":
             coll[0].nextElementSibling.style.display="flex";
